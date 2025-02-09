9810cb6f9f9d98dbe466c7a2b09e5dcc0efb345c
Mathieu Bouchonnet <mathieubouchonnet@gmail.com>
2025-02-09 20:18:22 +0100

    initial commit

---
+++ nvim/.config/nvim/after/ftplugin/c_sharp.lua
@@ -0,0 +1,5 @@
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.shiftwidth = 4 -- Indent size
+vim.opt.tabstop = 4 -- Tab size
+vim.opt.smartindent = true -- Smart indentation
+vim.opt.autoindent = true
---
+++ readme.md
@@ -0,0 +1,12 @@
+ripgrep
+fd (symlink needed on ubuntu) (sudo ln --symbolic $(which fdfind) /usr/local/bin/fd)
+fzf
+neovim
+build-essential
+tldr-pages
+python
+eza
+lazygit 
+starship
+z (smarter cd)
+diffnav
